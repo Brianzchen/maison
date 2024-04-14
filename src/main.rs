9e61400556c304381feae55c1a404b2bcb33b300
Brian C <bzchen93@gmail.com>
2024-04-15 08:57:12 +1000

    add a simple command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Repo maintenance CLI to help you keep your house in order
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The feature of Maison to invoke (ie: loc)
+    feature: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.feature == "loc" {
+        println!("you called loc!");
+    } else {
+        panic!("You must call a valid feature for maison to run upon, try `maison loc` as an example")
+    }
 }
