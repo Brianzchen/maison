f4b39c75fee230af1c20f6d48a298f6d9e23c3cf
Brian Chen <bzchen93@gmail.com>
2024-06-13 20:33:47 +1000

    can find all files in dir

--- src/loc.rs
+++ src/loc.rs
@@ -1,8 +1,18 @@
 use std::fs;
 
+// fn track_file(mut files: Vec<(&String)>, file_name: &String) {
+//     // Read the file into a string
+//     // count number of lines of code then store both that
+//     // and
+//     // let file_content = fs::read_to_string(path)?;
+//     files.push((file_name));
+// }
+
 pub fn run() {
     // Read the current directory
     let current_dir = fs::read_dir(".").unwrap();
+
+    let mut files: Vec<String> = Vec::new();
 
     for entry in current_dir {
         let entry = entry.unwrap();
@@ -10,20 +20,19 @@ pub fn run() {
 
         // Check if the entry is a file
         if path.is_file() {
-            // Read the file into a string
-            // let file_content = fs::read_to_string(path)?;
-
-            // // Print the file name and content
-            // println!("File: {}", copied_path.display());
-            println!("{}", path.display());
+            files.push(path.to_str().unwrap().to_string());
         } else {
           // Keep going and recursively find finds
           let nested_dir = fs::read_dir(entry.path()).unwrap();
           println!("{:?}", nested_dir);
 
-          for _entry in nested_dir {
-
+          for entry in nested_dir {
+            files.push(entry.unwrap().path().to_str().unwrap().to_string());
           }
         }
+    }
+
+    for (_index, file_name) in files.iter().enumerate() {
+        println!("{file_name}");
     }
 }
