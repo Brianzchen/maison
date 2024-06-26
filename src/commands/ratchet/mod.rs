0b6aef4ca62e2f8f910965cebbd0ff1906d4c578
Brian C <bzchen93@gmail.com>
2024-06-26 21:28:32 +1000

    write some logic

--- src/commands/ratchet/mod.rs
+++ src/commands/ratchet/mod.rs
@@ -1,5 +1,13 @@
 use clap::ArgMatches;
 
+/// Ratchet a value against a name
+/// that tags the latest commit with the given value
+///
+/// The value must be numeric signalling the number of errors acceptable
+/// If the value is greater we throw an error
+/// If the value is the same we do nothing
+/// If the value is lesser
+///     We should ...
 pub fn run(matches: &ArgMatches) {
     let name = matches.get_one::<String>("name").unwrap();
     println!("ratcheting {}", name);
